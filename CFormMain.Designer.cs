0f5404ed951eb08bc4e35997ccd2426137ad80b3
Youngmin322 <choyoungmin0322@gmail.com>
2024-10-17 09:48:55 +0900

    TCP/IP Chat Program

---
+++ CFormMain.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ChatClient
+{
+    partial class CFormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CFormMain));
+            this.grpConnect = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btConnect = new System.Windows.Forms.Button();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblIpAddr = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.timerReceive = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.grpConnect.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpConnect
+            // 
+            this.grpConnect.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("grpConnect.BackgroundImage")));
+            this.grpConnect.Controls.Add(this.button1);
+            this.grpConnect.Controls.Add(this.btConnect);
+            this.grpConnect.Controls.Add(this.lblPort);
+            this.grpConnect.Controls.Add(this.lblIpAddr);
+            this.grpConnect.Controls.Add(this.txtPort);
+            this.grpConnect.Controls.Add(this.txtIpAddress);
+            this.grpConnect.Location = new System.Drawing.Point(-2, 2);
+            this.grpConnect.Name = "grpConnect";
+            this.grpConnect.Size = new System.Drawing.Size(985, 143);
+            this.grpConnect.TabIndex = 0;
+            this.grpConnect.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(780, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 92);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btConnect
+            // 
+            this.btConnect.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btConnect.BackgroundImage")));
+            this.btConnect.Image = ((System.Drawing.Image)(resources.GetObject("btConnect.Image")));
+            this.btConnect.Location = new System.Drawing.Point(594, 25);
+            this.btConnect.Name = "btConnect";
+            this.btConnect.Size = new System.Drawing.Size(155, 92);
+            this.btConnect.TabIndex = 4;
+            this.btConnect.UseVisualStyleBackColor = true;
+            this.btConnect.Click += new System.EventHandler(this.btConnect_Click);
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.BackColor = System.Drawing.Color.Transparent;
+            this.lblPort.Font = new System.Drawing.Font("Baloo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPort.Location = new System.Drawing.Point(77, 80);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(59, 27);
+            this.lblPort.TabIndex = 3;
+            this.lblPort.Text = "Port :";
+            // 
+            // lblIpAddr
+            // 
+            this.lblIpAddr.AutoSize = true;
+            this.lblIpAddr.BackColor = System.Drawing.Color.Transparent;
+            this.lblIpAddr.Font = new System.Drawing.Font("Baloo", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIpAddr.Location = new System.Drawing.Point(24, 32);
+            this.lblIpAddr.Name = "lblIpAddr";
+            this.lblIpAddr.Size = new System.Drawing.Size(112, 27);
+            this.lblIpAddr.TabIndex = 2;
+            this.lblIpAddr.Text = "IP Address :";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(160, 82);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(175, 25);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "10050";
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(160, 34);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(175, 25);
+            this.txtIpAddress.TabIndex = 0;
+            this.txtIpAddress.Text = "220.67.174.71";
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtReceive.Location = new System.Drawing.Point(-2, 151);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtReceive.Size = new System.Drawing.Size(985, 592);
+            this.txtReceive.TabIndex = 1;
+            this.txtReceive.TextChanged += new System.EventHandler(this.txtReceive_TextChanged);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(-2, 749);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(790, 72);
+            this.txtSend.TabIndex = 2;
+            // 
+            // btSend
+            // 
+            this.btSend.Image = ((System.Drawing.Image)(resources.GetObject("btSend.Image")));
+            this.btSend.Location = new System.Drawing.Point(794, 749);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(189, 72);
+            this.btSend.TabIndex = 3;
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // timerReceive
+            // 
+            this.timerReceive.Tick += new System.EventHandler(this.timerReceive_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(843, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 64);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(721, 171);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 64);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnSendCommand_Click);
+            // 
+            // CFormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 831);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.grpConnect);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CFormMain";
+            this.Text = "Bubble Chat";
+            this.grpConnect.ResumeLayout(false);
+            this.grpConnect.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpConnect;
+        private System.Windows.Forms.Button btConnect;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblIpAddr;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.Timer timerReceive;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ CFormMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class CFormMain : Form
+    {
+        public TcpClient mTcpClient = null;
+        public NetworkStream mStream = null;
+
+        public CFormMain()
+        {
+            InitializeComponent();
+
+            // txtSend에 KeyDown 이벤트 핸들러 추가
+            txtSend.KeyDown += new KeyEventHandler(txtSend_KeyDown);
+        }
+
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            if (mTcpClient == null)
+            {
+                mTcpClient = new TcpClient(AddressFamily.InterNetwork);
+                IPAddress iPAddress = IPAddress.Parse(txtIpAddress.Text);
+                int port = Convert.ToInt32(txtPort.Text);
+
+                IPEndPoint iPEndPoint = new IPEndPoint(iPAddress, port);
+                try
+                {
+                    mTcpClient.Connect(iPEndPoint);
+                    timerReceive.Enabled = true;
+                    mStream = mTcpClient.GetStream();
+                }
+                catch (SocketException)
+                {
+                    MessageBox.Show("서버에 접속하세요!", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    mTcpClient = null;
+                }
+            }
+            else
+            {
+                mStream.Close();
+                mStream.Dispose();
+                mStream = null;
+
+                mTcpClient.Close();
+                mTcpClient.Dispose();
+                mTcpClient = null;
+
+                timerReceive.Enabled = false;
+            }
+        }
+
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            SendText();
+        }
+
+        // txtSend의 KeyDown 이벤트 핸들러
+        private void txtSend_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendText();
+
+                // 텍스트 전송 후 Enter 키의 기본 동작(예: 줄바꿈) 방지
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        // 텍스트 전송 코드를 별도의 메서드로 추출
+        private void SendText()
+        {
+            if (mTcpClient == null || mTcpClient.Connected == false)
+            {
+                MessageBox.Show("서버에 접속하세요!", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            byte[] data = Encoding.Default.GetBytes(txtSend.Text);
+            mTcpClient.Client.Send(data);
+
+            // 메시지 전송 후, 메시지 입력창 비우기
+            txtSend.Text = string.Empty;
+        }
+
+        private void timerReceive_Tick(object sender, EventArgs e)
+        {
+            if (mTcpClient == null || mTcpClient.Connected == false || mStream == null)
+            {
+                return;
+            }
+
+            if (mStream.DataAvailable == true)
+            {
+                byte[] bzReceive = new byte[1024];
+
+                mStream.ReadAsync(bzReceive, 0, 1024);
+
+                string strReceive = Encoding.Default.GetString(bzReceive);
+
+                StringBuilder sb = new StringBuilder(strReceive);
+
+                txtReceive.Text += sb;
+                txtReceive.AppendText("\r\n");
+            }
+        }
+
+        private void txtReceive_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtReceive.Text = string.Empty;
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (mTcpClient != null && mTcpClient.Connected)
+            {
+                mStream.Close();
+                mStream.Dispose();
+                mStream = null;
+
+                mTcpClient.Close();
+                mTcpClient.Dispose();
+                mTcpClient = null;
+
+                timerReceive.Enabled = false;
+            }
+        }
+
+        private void btnSendCommand_Click(object sender, EventArgs e)
+        {
+            if (mTcpClient != null && mTcpClient.Connected)
+            {
+                string command = "안녕하세요!"; // 전송할 명령
+                byte[] data = Encoding.Default.GetBytes(command);
+                mTcpClient.Client.Send(data);
+            }
+            else
+            {
+                MessageBox.Show("서버에 접속되지 않았습니다.", "오류", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
